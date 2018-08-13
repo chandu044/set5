efbc80f2e95963062f776db201023d97f6bb7465
chandu044 <41730209+chandu044@users.noreply.github.com>
2018-08-13 17:39:20 +0530

    Create strin.java

---
+++ strin.java
@@ -0,0 +1,28 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		String s1=sc.next();
+		String s2=sc.next();
+		int a=s1.compareTo(s2);
+	             
+	               if(a==0)
+	              {
+	    System.out.print(s2);
+	              
+	             
+	}
+	else 
+	{
+		System.out.print(s1);
+	}
+}
+}
