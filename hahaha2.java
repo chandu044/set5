ecda8e65c00e12955498d9ae36fe37b215d6da5e
chandu044 <41730209+chandu044@users.noreply.github.com>
2018-08-17 22:25:44 +0530

    Update and rename hahaha.java to hahaha2.java

--- hahaha2.java
+++ hahaha2.java
@@ -9,7 +9,7 @@ class Ideone
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
-		int n,i=2,c=0;
+		int n,i=1,c=0;
 		Scanner s=new Scanner(System.in);
 		n=s.nextInt();
 		while(i<=n)
