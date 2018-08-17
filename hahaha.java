f0be8fd808a9f6f21e3017653e7f1baed7c4cda1
chandu044 <41730209+chandu044@users.noreply.github.com>
2018-08-17 22:17:49 +0530

    Create hahaha.java

---
+++ hahaha.java
@@ -0,0 +1,33 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int n,i=2,c=0;
+		Scanner s=new Scanner(System.in);
+		n=s.nextInt();
+		while(i<=n)
+		{
+			if(i==n)
+			{
+				c=c+1;
+			}
+			i=i*2;
+		}
+		if(c==1)
+		{
+		        	System.out.print("yes");
+			
+		}
+		else
+		{
+			System.out.print("no");
+		}
+	}
+}
